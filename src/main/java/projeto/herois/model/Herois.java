ac4bbe054606f0b6787cd8d641428627111d08af
Mateus Silva <mateushenrique30954@gmail.com>
2021-08-18 17:27:27 -0300

    Criando a classe Heroi

--- src/main/java/projeto/herois/model/Herois.java
+++ src/main/java/projeto/herois/model/Herois.java
@@ -1,5 +1,60 @@
 package projeto.herois.model;
 
-public class Herois {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+public class Herois implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	@Type(type = "uuid-char") @Column(length = 36)
+	private UUID codigo;
+	
+	public UUID getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(UUID codigo) {
+		this.codigo = codigo;
+	}
+	private String nome;
+	private String poder;
+	private String universo;
+	private LocalDate dataCadastro;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPoder() {
+		return poder;
+	}
+	public void setPoder(String poder) {
+		this.poder = poder;
+	}
+	public String getUniverso() {
+		return universo;
+	}
+	public void setUniverso(String universo) {
+		this.universo = universo;
+	}
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
 }
