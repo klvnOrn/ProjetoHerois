2915adcadbc5ae42dca7355a1c60fba172046763
raigc <16510382+raigc@users.noreply.github.com>
2021-08-20 17:36:24 -0300

    Buscar e remover Universos por Id

--- src/main/java/projeto/herois/controller/UniversosController.java
+++ src/main/java/projeto/herois/controller/UniversosController.java
@@ -3,16 +3,46 @@ package projeto.herois.controller;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import projeto.herois.model.Universos;
 import projeto.herois.repository.CrudUniversos;
 
+@RestController
 public class UniversosController {
+	
 	@Autowired
-	private CrudUniversos ur;
-
+	CrudUniversos cu;
+	
+	@GetMapping("/universos")
+	public Iterable<Universos> getAllUniversos(){
+		return cu.findAll();
+		
+	}
+	
+	@GetMapping("/universos/{idUniverso}")
+	public Universos getUniversosById(@PathVariable("idUniverso") UUID idUniverso) {
+		Universos universo = this.cu.findById(idUniverso).orElse(null);
+		return universo;
+	}
+	
+	@PostMapping("/cadastrarUniversos")
+	public Universos saveUniversos(@RequestBody Universos universos) {
+		return cu.save(universos);
+	}
+//	
+//	@DeleteMapping("/deletarHeroi/{id}")
+//	public void deletarHeroi(@PathVariable String id) {
+//		ch.deleteById(id);
+//	}
+	
+	@DeleteMapping("/deletarUniverso/{idUniverso}")
+	public void deleteUniversosById(@PathVariable("idUniverso") UUID idUniverso) {
+		this.cu.deleteById(idUniverso);
+	}
 }
--- src/main/java/projeto/herois/repository/CrudUniversos.java
+++ src/main/java/projeto/herois/repository/CrudUniversos.java
@@ -2,18 +2,18 @@ package projeto.herois.repository;
 
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import projeto.herois.model.Herois;
 import projeto.herois.model.Universos;
 
 
 
-
 @Repository
-public interface CrudUniversos extends CrudRepository<Universos, Integer> {
-	CrudUniversos findByIdUniverso(UUID idUniverso);
+public interface CrudUniversos extends CrudRepository<Universos, UUID> {
+	Optional<Universos> findById(UUID idUniverso);
+	void deleteById(UUID idUniverso);
 }
