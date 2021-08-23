8d2f0595aaf04f9e2d71068c58dc38d7175b2ba5
Mateus Silva <mateushenrique30954@gmail.com>
2021-08-23 11:13:06 -0300

    Implementando classes poderes

---
+++ src/main/java/projeto/herois/controller/PoderesController.java
@@ -0,0 +1,43 @@
+package projeto.herois.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import projeto.herois.model.Poderes;
+import projeto.herois.repository.CrudPoderes;
+
+@RestController
+public class PoderesController {
+	
+	@Autowired
+	CrudPoderes cp;
+	
+	@GetMapping("/poderes")
+	public Iterable<Poderes> getAllPoderes(){
+		return cp.findAll();
+		
+	}
+	
+	@GetMapping("/poder/{idPoder}")
+	public Poderes getPoderesById(@PathVariable("idPoder") UUID idPoder) {
+		Poderes poder = this.cp.findById(idPoder).orElse(null);
+		return poder;
+	}
+	
+	@PostMapping("/cadastrarPoder")
+	public Poderes savePoder(@RequestBody Poderes poder) {
+		return cp.save(poder);
+	}
+	
+	@DeleteMapping("/deletarPoder/{idPoder}")
+	public void deletePoderById(@PathVariable("idPoder") UUID idPoder) {
+		this.cp.deleteById(idPoder);
+	}
+}
--- src/main/java/projeto/herois/repository/CrudPoderes.java
+++ src/main/java/projeto/herois/repository/CrudPoderes.java
@@ -2,16 +2,19 @@ package projeto.herois.repository;
 
 
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import projeto.herois.model.Herois;
 import projeto.herois.model.Poderes;
 
 
 
 
 @Repository
-public interface CrudPoderes extends CrudRepository<Poderes, Integer> {
-	CrudPoderes findByNomePoder(String nomePoder);
+public interface CrudPoderes extends CrudRepository<Poderes, UUID> {
+	Optional<Poderes> findById(UUID idPoderes);
+	void deleteById(UUID idPoderes);
 }
