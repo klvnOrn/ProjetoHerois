d2680ecc804969849409bfe2eedf3645193d45f5
Mateus Silva <mateushenrique30954@gmail.com>
2021-08-23 10:24:36 -0300

    Alterações nos métodos

--- src/main/java/projeto/herois/repository/CrudHerois.java
+++ src/main/java/projeto/herois/repository/CrudHerois.java
@@ -1,5 +1,9 @@
 package projeto.herois.repository;
 
+
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +11,7 @@ import projeto.herois.model.Herois;
 
 
 @Repository
-public interface CrudHerois extends CrudRepository<Herois, String> {
-	
+public interface CrudHerois extends CrudRepository<Herois, UUID> {
+	Optional<Herois> findById(UUID idHerois);
+	void deleteById(UUID idHerois);
 }
