f46c9574499eea18a89fe1e29b8f0ef9240a5348
Mateus Silva <mateushenrique30954@gmail.com>
2021-08-24 14:53:34 -0300

    Implementando Update nas Classes Controller de Poderes e Universos

--- src/main/java/projeto/herois/controller/UniversosController.java
+++ src/main/java/projeto/herois/controller/UniversosController.java
@@ -2,11 +2,14 @@ package projeto.herois.controller;
 
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +43,10 @@ public class UniversosController {
 	public void deleteUniversoById(@PathVariable("idUniverso") UUID idUniverso) {
 		this.cu.deleteById(idUniverso);
 	}
+		
+	@PutMapping("/atualizarUniverso")
+	public Universos updateUniverso(@RequestBody Universos universo) {
+		return cu.save(universo);
+	}
+	
 }
