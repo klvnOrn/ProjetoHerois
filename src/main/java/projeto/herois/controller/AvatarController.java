4e96a0aee735f1eadc46fe3fc25757ebf432ca14
Mateus Silva <mateushenrique30954@gmail.com>
2021-08-30 18:18:22 -0300

    Deletar avatar

--- src/main/java/projeto/herois/controller/AvatarController.java
+++ src/main/java/projeto/herois/controller/AvatarController.java
@@ -3,12 +3,16 @@ package projeto.herois.controller;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import projeto.herois.model.Avatar;
+import projeto.herois.payload.ApiResponse;
 import projeto.herois.repository.CrudAvatar;
 import projeto.herois.repository.FileHelper;
 
@@ -34,6 +39,20 @@ public class AvatarController {
 	public AvatarController(FileHelper fileHelper) {
         this.fileHelper = fileHelper;
     }
+	
+	@GetMapping("/avatares")
+	public Iterable<Avatar> getAllAvatares(){
+		return ca.findAll();
+	}
+	
+	@GetMapping("/avatar/{idAvatar}")
+	public Avatar getAvatarById(@PathVariable("idAvatar") UUID idAvatar) throws NotFoundException {
+			Avatar avatar = this.ca.findById(idAvatar).orElse(null);
+			if (avatar == null) {
+				throw new NotFoundException();
+			}
+			return avatar;
+	}
 	
     @GetMapping("/downloadAvatar/{nomeAvatar}")
     @ResponseBody
@@ -59,5 +78,25 @@ public class AvatarController {
         ca.save(avatar);
 		return avatar;
     }
-	
+    
+    @DeleteMapping("/deletarAvatar/{idAvatar}")
+	public ResponseEntity<?> ResponseEntity (@PathVariable("idAvatar") UUID idAvatar) throws NotFoundException{
+		Avatar avatar = this.ca.findById(idAvatar).orElse(null);
+		if (avatar == null) {
+			throw new NotFoundException();
+		}
+		else {
+			this.ca.deleteById(idAvatar);
+			return new ResponseEntity<Object>(new ApiResponse(true, "Avatar deletado!"),new HttpHeaders(), HttpStatus.OK);
+		}
+	}
+    
+//    @PutMapping("/atualizarAvatar/{idAvatar}")
+//	public ResponseEntity<?> updateAvatar(@RequestParam("file") MultipartFile file, Avatar avatar) throws NotFoundException, IOException{
+//    	Avatar avatarFile = new Avatar();
+//    	avatarFile = uploadFile(file, avatarFile);
+//		getAvatarById(avatar.getIdAvatar());
+//		ca.save(avatar);
+//		return new ResponseEntity<Object>(new ApiResponse(true, "Avatar atualizado!"),new HttpHeaders(), HttpStatus.OK);
+//	}
 }
